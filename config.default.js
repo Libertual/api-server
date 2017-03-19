ab76f3900578a4feedef002ba7f79d9b277bb5b8
Libertual <peterworld@gmail.com>
2017-03-20 00:21:22 +0100

    Add config file to gitignore and create a confif.default.js

--- config.default.js
+++ config.default.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
-  SECRET_TOKEN: 'L4s c0$4$ lm0rtnts n* s0n k0s4s'
+  SECRET_TOKEN: 'miclavedetoken'
 }

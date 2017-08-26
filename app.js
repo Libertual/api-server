e24adec68535f6710dcff65ff890c8e3a3282c51
Pedro Macías <peterworld@gmail.com>
2017-08-26 10:54:10 +0200

    Correct Styles
    New rules in .eslintrc file to validate indentation on variables.

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-'use strict'
 
-const express     = require('express')
-const bodyParser  = require('body-parser')
-const logger      = require('morgan')
-const cors        = require('cors')
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const logger        = require('morgan');
+const cors          = require('cors');
+
 
 // Routes
-const api         = require('./routes/index')
-const authRoutes  = require('./routes/authRoutes')
-const accountRoutes  = require('./routes/accountRoutes')
+const api           = require('./routes/index');
+const authRoutes    = require('./routes/authRoutes');
+const accountRoutes = require('./routes/accountRoutes');
 
-let app = express();
+const app = express();
 
-app.enable("jsonp callback");
+app.enable('jsonp callback');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 app.use(logger('dev'));
 app.use('/api', api);
 app.use('/auth', authRoutes);
 app.use('/account', accountRoutes);
 
-module.exports = app
+module.exports = app;

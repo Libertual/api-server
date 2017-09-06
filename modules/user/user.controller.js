328d40a0a83a909ed9a22bcedcf656599d3e9851
Pedro Macías <peterworld@gmail.com>
2017-09-06 21:41:53 +0200

    Refactor deleting logs and traces

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -8,20 +8,20 @@ function getFollowers(req, res) {
   User.findOne({ userName: req.baseUrl.substr(1) })
     .populate('followers')
     .exec((err, user) => {
-      console.log(user);
+      // console.log(user);
       if (err) return res.status(500).send({ message: `Error, ${err}` });
       return res.status(200).send({ message: 'Request accepted', user });
     });
 }
 
 function getFriends(req, res) {
-  console.log('Get friends');
-  console.log(req.body);
+  // console.log('Get friends');
+  // console.log(req.body);
 
   User.findOne({ userName: req.baseUrl.substr(1) })
     .populate('friends')
     .exec((err, user) => {
-      console.log(user);
+      // console.log(user);
       if (err) return res.status(500).send({ message: `Error, ${err}` });
       return res.status(200).send({ message: 'Request accepted', user });
     });

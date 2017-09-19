e6dfff0072eacea2ff0fb25ed9b5f92a1b5ff624
Pedro Macías <peterworld@gmail.com>
2017-09-19 18:32:02 +0200

    Test mongodb server

--- config/main.default.js
+++ config/main.default.js
@@ -2,6 +2,6 @@ module.exports = {
   EXPIRATION_DAYS: 7,
   SERVER: '0.0.0.0',
   PORT: process.env.PORT || 3000,
-  DB: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+  DB: process.env.MONGODB || 'mongodb://192.168.1.150:27017/shop',
   SECRET_TOKEN: 'miclavedetoken'
 };

abfb028e81952e264ba715e931561356e8420795
Pedro Macías <peterworld@gmail.com>
2017-09-22 13:21:06 +0200

    Test config

--- config/main.js
+++ config/main.js
@@ -3,5 +3,5 @@ module.exports = {
   SERVER: '0.0.0.0',
   PORT: process.env.PORT || 3000,
   DB: process.env.MONGODB || 'mongodb://localhost:27017/api-server',
-  SECRET_TOKEN: 'frase muy sercretísima'
+  SECRET_TOKEN: 'frase sercretísima'
 };

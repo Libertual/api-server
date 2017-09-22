3175aa898aa112b41fef3672dc745d7993558dfb
Pedro Macías <peterworld@gmail.com>
2017-09-22 13:09:03 +0200

    Ignore some files

--- config/main.js
+++ config/main.js
@@ -3,5 +3,5 @@ module.exports = {
   SERVER: '0.0.0.0',
   PORT: process.env.PORT || 3000,
   DB: process.env.MONGODB || 'mongodb://localhost:27017/api-server',
-  SECRET_TOKEN: 'L4s c0$4$ lm0rtnts n* s0n k0s4s'
+  SECRET_TOKEN: 'palabra secretisima'
 };

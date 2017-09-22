48cc4e812cf524adc4106dfc37ee7c8b31ad99f4
Pedro Macías <peterworld@gmail.com>
2017-09-22 13:12:59 +0200

    Change config

--- config/main.js
+++ config/main.js
@@ -3,5 +3,5 @@ module.exports = {
   SERVER: '0.0.0.0',
   PORT: process.env.PORT || 3000,
   DB: process.env.MONGODB || 'mongodb://localhost:27017/api-server',
-  SECRET_TOKEN: 'palabra secretisima'
+  SECRET_TOKEN: 'frase muy sercretísima'
 };
